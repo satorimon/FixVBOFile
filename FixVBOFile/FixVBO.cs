4cf80951c6ba6cd1cdda84901953f27210e7e573
satorimon <48250071+satorimon@users.noreply.github.com>
2022-03-01 17:23:09 +0900

    first commit

---
+++ FixVBOFile/FixVBO.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace FixVBOFile
+{
+    public class FixVBO
+    {
+        public static  void DoFix(string inputpath, string outputpath)
+        {
+            if(!File.Exists(inputpath))
+            {
+                throw new ArgumentException("file not found.");
+            }
+            if(Path.GetExtension(inputpath) != ".vbo")
+            {
+                throw new ArgumentException("Invalid file.");
+            }
+            using (var sr = new StreamReader(inputpath, Encoding.UTF8))
+            using(var sw = new StreamWriter(outputpath, false, Encoding.UTF8 ))
+            {
+                sw.AutoFlush = true;
+                var line = "";
+                do
+                {
+                    line = sr.ReadLine();
+                    sw.WriteLine(line);
+                }
+                while (!line.StartsWith("[column names]"));
+
+                var columnNames = sr.ReadLine();
+
+                var splitNames = columnNames.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+                var index = splitNames.Select((item, index) => (item, index))
+                    .Where(pare => string.Compare(pare.item, "time", true) == 0)
+                    .FirstOrDefault().index;
+
+                sw.WriteLine(columnNames);
+                do
+                {
+                    line = sr.ReadLine();
+                    sw.WriteLine(line);
+                }
+                while (!line.StartsWith("[data]"));
+
+                do
+                {
+                    line = sr.ReadLine();
+                    var lineSplited = line.Split(" ", StringSplitOptions.None);
+                    if (lineSplited.Length > index && !lineSplited[index].EndsWith('0'))
+                    {
+                        lineSplited[index] = lineSplited[index].Substring(0, lineSplited[index].Length - 1) + "0";
+                    }
+                    
+
+                    sw.WriteLine(string.Join(" ", lineSplited));
+                }
+                while (!sr.EndOfStream);
+
+                sr.Close();
+                sw.Flush();
+                sw.Close();
+
+            }
+        }
+
+        
+    }
+}
---
+++ FixVBOFile/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+
+namespace FixVBOFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 1)
+            {
+                Console.WriteLine("引数が有りません。処理を終了します。");
+                return;
+            }
+            foreach (var s in args)
+            {
+                Console.WriteLine(s);
+                var inputPath = s;
+                var filename = Path.GetFileNameWithoutExtension(inputPath);
+                filename += "_fixed";
+                var outputPath = Path.GetDirectoryName(inputPath) + Path.DirectorySeparatorChar
+                    + filename + Path.GetExtension(inputPath);
+
+                FixVBO.DoFix(inputPath, outputPath);
+            }
+
+
+
+
+        }
+    }
+}
